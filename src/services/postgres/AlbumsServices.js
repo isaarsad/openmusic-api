ec148b80897a7072f5a307fc269b617dfac33b92
isa-arsad <isaarsad0@gmail.com>
2026-01-05 13:51:00 +0700

    fix nanoid on albums

--- src/services/postgres/AlbumsServices.js
+++ src/services/postgres/AlbumsServices.js
@@ -9,7 +9,7 @@ class AlbumsService {
   }
 
   async addAlbum({ name, year }) {
-    const id = nanoid(16);
+    const id = `album-${nanoid(16)}`;
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
